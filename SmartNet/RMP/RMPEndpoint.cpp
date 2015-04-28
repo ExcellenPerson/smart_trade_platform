9b2b8057c6a547c59de4167ef320aecae668d9f3
TheRockLiuHY <rockjohnson@163.com>
2015-04-28 10:38:01 -0400

    md

---
+++ SmartNet/RMP/RMPEndpoint.cpp
@@ -0,0 +1,21 @@
+/*
+ * RMPEndpoint.cpp
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: rock
+ */
+
+#include "RMPEndpoint.h"
+
+namespace NSSmartNet {
+
+CRMPEndpoint::CRMPEndpoint() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CRMPEndpoint::~CRMPEndpoint() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NSSmartNet */
---
+++ SmartNet/RMP/RMPEndpoint.h
@@ -0,0 +1,21 @@
+/*
+ * RMPEndpoint.h
+ *
+ *  Created on: Apr 28, 2015
+ *      Author: rock
+ */
+
+#ifndef RMP_RMPENDPOINT_H_
+#define RMP_RMPENDPOINT_H_
+
+namespace NSSmartNet {
+
+class CRMPEndpoint {
+public:
+	CRMPEndpoint();
+	virtual ~CRMPEndpoint();
+};
+
+} /* namespace NSSmartNet */
+
+#endif /* RMP_RMPENDPOINT_H_ */
