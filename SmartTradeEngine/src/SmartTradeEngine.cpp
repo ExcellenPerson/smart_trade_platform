5a6e193938e545ad090756f9ea39582fc227e527
TheRockLiuHY <rockjohnson@163.com>
2015-04-28 11:15:36 -0400

    md

---
+++ SmartTradeEngine/src/SmartTradeEngine.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SmartTradeEngine.cpp
+// Author      : TheRockLiuHY
+// Version     :
+// Copyright   : liuhongyang's copyright
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
