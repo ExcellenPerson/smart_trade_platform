ebed5e3d82496265daeb32172968dafeb338eb22
TheRockLiuHY <rockjohnson@163.com>
2015-05-08 09:22:14 +0800

    modify

---
+++ TestOpenCL/src/TestOpenCL.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TestOpenCL.c
+ Author      : TheRockLiuHY
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello UPC World"); /* prints Hello UPC World */
+	return EXIT_SUCCESS;
+}
---
+++ TestUPC/src/TestUPC.upc
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Name        : TestUPC.upc
+ Author      : TheRockLiuHY
+ Version     :
+ Copyright   : Your copyright notice
+ Description : UPC Hello world program
+ ============================================================================
+*/
+#include <upc.h>
+
+int main(int argc, char *argv[]) {
+	printf("Hello, I am %d of %d.\n", MYTHREAD, THREADS);
+	return 0;
+}
