13bd4fd63909157d8f37762298ecd2884e25c8c7
TheRockLiuHY <rockjohnson@163.com>
2015-05-15 15:18:49 +0800

    mm

---
+++ build/STPTest/CMakeFiles/STPTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STPTest.dir/src/STPTest.cpp.o"
+  "STPTest.pdb"
+  "STPTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STPTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/SmartUtils/CMakeFiles/SmartUtils.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartUtils.dir/Utils/EventNotifier.cpp.o"
+  "libSmartUtils.pdb"
+  "libSmartUtils.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartUtils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
