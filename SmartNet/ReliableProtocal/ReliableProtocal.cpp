513d2afacdea06f2dd415086cebae3164158132e
TheRockLiuHY <rockjohnson@163.com>
2015-05-18 17:30:44 +0800

    mm

--- SmartNet/ReliableProtocal/ReliableProtocal.cpp
+++ SmartNet/ReliableProtocal/ReliableProtocal.cpp
@@ -9,11 +9,66 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <vector>
 //#include <impl/framework.h>
 #include "ReliableProtocal.h"
 
 namespace
 {
+	typedef std::vector<byte_t> IPNumAddr_t;
+	typedef std::vector<IPNumberAddr_t> IPNumAddrList_t;
+
+
+	int32_t JoinGroup(int32_t Sock, const IPNumAddr_t &MulticastIP) const
+	{
+		if (-1 == Sock)
+		{
+
+		}
+
+		switch (group_address.size())
+		{
+			case kIPv4AddressSize:
+			{
+				if (addr_family_ != AF_INET)
+					return ERR_ADDRESS_INVALID;
+
+	#if !defined(OS_MACOSX)
+				ip_mreqn mreq;
+				mreq.imr_ifindex = multicast_interface_;
+				mreq.imr_address.s_addr = htonl(INADDR_ANY);
+	#else
+				ip_mreq mreq;
+				int error = GetIPv4AddressFromIndex(socket_, multicast_interface_,
+						&mreq.imr_interface.s_addr);
+				if (error != OK)
+				return error;
+	#endif
+				memcpy(&mreq.imr_multiaddr, &group_address[0], kIPv4AddressSize);
+				int rv = setsockopt(socket_, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq));
+				if (rv < 0)
+					return MapSystemError(errno);
+				return OK;
+			}
+			case kIPv6AddressSize:
+			{
+				if (addr_family_ != AF_INET6)
+					return ERR_ADDRESS_INVALID;
+				ipv6_mreq mreq;
+				mreq.ipv6mr_interface = multicast_interface_;
+				memcpy(&mreq.ipv6mr_multiaddr, &group_address[0], kIPv6AddressSize);
+				int rv = setsockopt(socket_, IPPROTO_IPV6, IPV6_JOIN_GROUP, &mreq, sizeof(mreq));
+				if (rv < 0)
+					return MapSystemError(errno);
+				return OK;
+			}
+			default:
+				NOTREACHED() << "Invalid address family";
+				return ERR_ADDRESS_INVALID;
+		}
+	}
+
+
 
 	static uint8_t pgm_in_mask2len(const struct in_addr*);
 	static uint8_t pgm_in6_mask2len(const struct in6_addr*);
